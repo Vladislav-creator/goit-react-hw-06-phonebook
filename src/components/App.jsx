adf97239eadf8d33c2c84747824de68f10206085
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-11-18 17:19:30 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { ToastContainer } from 'react-toastify';
+
+import { ContactForm } from '../components/ContactForm/ContactForm';
+import { ContactList } from '../components/ContactList/ContactList';
+import { Filter } from '../components/Filter/Filter';
+import {
+  MainContainer,
+  MainTitle,
+  ContactsTitle,
+  FilterTitle,
+} from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ToastContainer />
+      <MainContainer>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm />
+        <ContactsTitle>Contacts</ContactsTitle>
+        <FilterTitle>Find contacts by name</FilterTitle>
+        <Filter />
+        <ContactList />
+      </MainContainer>
+    </>
   );
 };
